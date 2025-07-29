46bbf02ba51812b1cc5f9d93aae4c9a0bc32180e
octakitten <raspberry2425@gmail.com>
2025-07-29 12:45:01 -0400

    continue work on parsing log files

--- parse-logs.py
+++ parse-logs.py
@@ -1,10 +1,12 @@
-import os
 import glob
 import re
 from file_read_backwards import FileReadBackwards
+import pathlib
 
 main():
 
+    # read through all the log files written during model training and extract info about the victories
+    # and saved models.
     filenames = glob.glob("./logs/*log.txt")
     phrase1 = re.compile(r'victory! it took this many turns:')
     phrase2 = "victory! a winning model was found! it took this many iterations:"
@@ -18,3 +20,20 @@ main():
             for line in lines:
                 if phrase1.search(line):
                     victories.append((lines[1], lines[3]))
+
+    # then save that info to disk in a summary file which we can read later
+    # the lines in the summary file alternate between 
+    # the number of turns a victory took in the game
+    # and the number of iterations it took to find a winning model that run
+    output = pathlib.Path("./logs/summary.txt")
+    directory = pathlib.Path("./logs")
+    backup = pathlib.Path("./logs/summary-prev.txt")
+    
+    if directory.exists():
+        if output.exists():
+            output.replace(backup)
+        output.touch()
+        with output.open() as summary:
+            for entry in victories:
+                output.writeline(entry[0])
+                output.writeline(entry[1])
